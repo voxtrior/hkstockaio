b188fee66bcf36b82c00501057199b34f1d6d49f
voxtrior <voxtrior@gmail.com>
2025-04-10 08:42:43 +0800

    hell

--- __init__.py
+++ __init__.py
@@ -1,1 +1,6 @@
-print("hello world")
+import logging
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    return func.HttpResponse("Hello World from Azure Functions!")
